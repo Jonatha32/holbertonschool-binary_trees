50f4668d2aded35f137c0543ddfccb190593f8df
Jonatha32 <jonaperez038@gmail.com>
2024-08-07 17:09:11 +0000

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,27 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor
- * @tree: pointer to the nodeto measure
- * Return: balance factor
+ * binary_tree_height - binary tree height
+ * @tree: parameter
+ * Return: 0, a + 1 or b + 1
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t a, b;
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (0);
+a = binary_tree_height(tree->left);
+b = binary_tree_height(tree->right);
+if (a > b)
+return (a + 1);
+else
+return (b + 1);
+}
+
+/**
+ * binary_tree_balance - balance
+ * @tree: parameter
+ * Return: a - b
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int depth_l = 0;
-	int depth_r = 0;
-	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
-
-	if (tree == NULL || temp == NULL)
-		return (0);
-	if (tree->left != NULL)
-	{
-		for (temp = tree->left; temp != NULL; temp = temp->left)
-			depth_l++;
-	}
-	if (tree->right != NULL)
-	{
-		for (temp = tree->right; temp != NULL; temp = temp->right)
-			depth_r++;
-	}
-	return (depth_l - depth_r);
+int a, b;
+if (tree == NULL)
+return (0);
+a = (tree->left) ? (int)binary_tree_height(tree->left) : -1;
+b = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
+return (a - b);
 }

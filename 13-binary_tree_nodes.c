33a4c8b60b64abc4918df2674722a8acd8025b84
Jonatha32 <jonaperez038@gmail.com>
2024-08-07 02:07:38 +0000

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,9 +6,9 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+size_t a = 0;
 if (tree == NULL)
 return (0);
-size_t a = 0;
 if (tree->left != NULL || tree->right != NULL)
 a = 1;
 a += binary_tree_nodes(tree->left);

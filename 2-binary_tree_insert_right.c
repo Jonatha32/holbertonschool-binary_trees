88aee9bceea72626609a2f9b0ce791e6c93a5f55
hexNulgath <machadoignacio@hotmail.com>
2024-08-07 13:44:20 +0000

    fixed errors

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -22,6 +22,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 		parent->right->parent = new_node_right;
 	parent->right = new_node_right;
-	
+
 	return (new_node_right);
 }
